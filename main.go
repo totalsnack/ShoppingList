0c81d6b595b10004d996b3a66b6336998492e447
Eugene <Jiztastamablastamarang@gmail.com>
2022-06-15 10:14:20 +0300

    push

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		applePrice = 5_99
+		pearPrice  = 7_00
+	)
+	walletSize := 23_00
+
+	basket := func(applesNumber, pearsNumber int) int {
+		return applesNumber*applePrice + pearsNumber*pearPrice
+	}
+
+	shoppingList := map[string]interface{}{
+		"Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?": float64(basket(9, 8)) / 100,
+		"Скільки груш ми можемо купити?":                                walletSize / pearPrice,
+		"Скільки яблук ми можемо купити?":                               walletSize / applePrice,
+		"Чи ми можемо купити 2 груші та 2 яблука?":                      walletSize-basket(2, 2) > 0,
+	}
+	for k, v := range shoppingList {
+		fmt.Println(k, "==>", v)
+	}
+
+}
