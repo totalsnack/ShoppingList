121f1f0312114f89f1de7844c2cc138ffff22e0d
jiztastamablastamarang <jiztastamablastamarang@gmail.com>
2022-06-16 09:28:28 +0300

    redesign overengineering

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type Fruit struct {
+	price    int
+	quantity int
+}
+
+func basketOfFruits(F ...Fruit) (total int) {
+	for _, f := range F {
+		total += f.price * f.quantity
+	}
+	return
+}
+
 func main() {
 	const (
 		applePrice = 5_99
@@ -9,15 +21,11 @@ func main() {
 	)
 	walletSize := 23_00
 
-	basket := func(applesQuantity, pearsQuantity int) int {
-		return applesQuantity*applePrice + pearsQuantity*pearPrice
-	}
-
 	shoppingList := map[string]interface{}{
-		"Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?": float64(basket(9, 8)) / 100,
+		"Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?": float64(basketOfFruits(Fruit{applePrice, 9}, Fruit{pearPrice, 9})) / 100,
 		"Скільки груш ми можемо купити?":                                walletSize / pearPrice,
 		"Скільки яблук ми можемо купити?":                               walletSize / applePrice,
-		"Чи ми можемо купити 2 груші та 2 яблука?":                      walletSize-basket(2, 2) > 0,
+		"Чи ми можемо купити 2 груші та 2 яблука?":                      walletSize-basketOfFruits(Fruit{applePrice, 2}, Fruit{pearPrice, 2}) >= 0,
 	}
 	for k, v := range shoppingList {
 		fmt.Println(k, "==>", v)
