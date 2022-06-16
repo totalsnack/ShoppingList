53ada075b0f70d7fc274ca5aabbf915a43712527
Eugene <Jiztastamablastamarang@gmail.com>
2022-06-16 09:31:26 +0300

    push

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ type Fruit struct {
 	quantity int
 }
 
-func basketOfFruits(F ...Fruit) (total int) {
+func basketOfFruits(F ...*Fruit) (total int) {
 	for _, f := range F {
 		total += f.price * f.quantity
 	}
@@ -22,10 +22,10 @@ func main() {
 	walletSize := 23_00
 
 	shoppingList := map[string]interface{}{
-		"Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?": float64(basketOfFruits(Fruit{applePrice, 9}, Fruit{pearPrice, 9})) / 100,
+		"Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?": float64(basketOfFruits(&Fruit{applePrice, 9}, &Fruit{pearPrice, 9})) / 100,
 		"Скільки груш ми можемо купити?":                                walletSize / pearPrice,
 		"Скільки яблук ми можемо купити?":                               walletSize / applePrice,
-		"Чи ми можемо купити 2 груші та 2 яблука?":                      walletSize-basketOfFruits(Fruit{applePrice, 2}, Fruit{pearPrice, 2}) >= 0,
+		"Чи ми можемо купити 2 груші та 2 яблука?":                      walletSize-basketOfFruits(&Fruit{applePrice, 2}, &Fruit{pearPrice, 2}) >= 0,
 	}
 	for k, v := range shoppingList {
 		fmt.Println(k, "==>", v)
